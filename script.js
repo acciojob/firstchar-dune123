ebf97bcd87ab69cf4d7adc6c35a65031a6589e6b
dune123 <96974209+dune123@users.noreply.github.com>
2024-10-31 13:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstChar(text) {
   // your code here
+	let x=text.trim();
+	if(text=="") return "";
+	return x.charAt(0).toUpperCase()
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+const text = prompt("Enter text:");   
+alert(firstChar(text));     
